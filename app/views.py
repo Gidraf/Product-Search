bcb29b0a9494bd3ac367d71ded234fa1a1d91d27
orenjaa <gidraf.orenjaa@sparepap.com>
2021-03-22 02:49:08 +0300

    fix routing

--- app/views.py
+++ app/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.template import loader
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    # context = {
+    #     'latest_question_list': latest_question_list,
+    # }
+    return render(request,"index.html")
--- product_search/urls.py
+++ product_search/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/search', include('app.urls'))
+    path('search/', include('app.urls'))
 ]
